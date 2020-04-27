2c66730c03393a6fc6cd06959affc92962244778
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-27 21:00:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "#{holiday}:"
+      puts "#{holiday.capitalize}:"
       supplies.each do |supply|
         puts "#{supply}"
       end
