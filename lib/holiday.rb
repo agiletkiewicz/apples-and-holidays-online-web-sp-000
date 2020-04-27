6b78d6f5a3ede07014fc331a1f7363cb8e88e1bf
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-27 21:37:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_array = []
   holiday_hash.each do |season, holidays|
-    season.each do |holiday, supplies|
+    holidays.each do |holiday, supplies|
       if supplies.values.include?("BBQ")
         holiday_array << holiday 
       end
